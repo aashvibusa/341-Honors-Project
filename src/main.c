123d81ca7fdd1f73b4aa5393be7bee52378ab712
Ronit Rout <122765671+ronit127@users.noreply.github.com>
2025-04-25 15:14:21 -0500

    Update main.c
    actually updated vocoder.c with error redirection this time

--- src/main.c
+++ src/main.c
@@ -5,6 +5,16 @@
 
 int main() {
     initialize_vocoder();
+
+    int fd = open("error.log", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    
+    if(fd == -1){
+        perror("unable to write");
+        exit(2);
+    }
+
+    dup2(fd, STDERR_FILENO);
+    close(fd);   
     
     if (init_audio() != 0) {
         fprintf(stderr, "Audio initialization failed!\n");
